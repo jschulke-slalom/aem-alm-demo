cbaf2bd08189df8aabe09f41983ed2f5d519be06
jschulke-slalom <jacob.schulke@slalom.com>
2024-11-12 14:50:53 -0600

    remove flip

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,41 +2,24 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* Change to ul, li */
+  /* change to ul, li */
   const ul = document.createElement('ul');
 
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
-
-    while (row.firstElementChild) {
-      li.append(row.firstElementChild);
-    }
-
+    while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        div.className = 'cards-card-image';
-      } else {
-        div.className = 'cards-card-body test';
-      }
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
-
-    // Add click event listener to toggle the 'flip' class
-    li.addEventListener('click', () => {
-      console.log('List item clicked:', li); // Add logging
-      li.classList.toggle('flip');
-      console.log('Current classes:', li.className); // Log current classes
-    });
-
     ul.append(li);
   });
-
   ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, { width: '750' });
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
-
   block.textContent = '';
   block.append(ul);
 }
