1d8407ac82bcea6334515e25dcf10b9b8c56d691
jschulke-slalom <jacob.schulke@slalom.com>
2024-11-15 09:05:36 -0600

    removed logs and unneed referance

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,4 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
@@ -16,26 +15,18 @@ export default function decorate(block) {
     moveInstrumentation(row, li);
     while (row.firstElementChild) {
       li.append(row.firstElementChild);
-      console.log('Appended child element to <li>');
     }
     const divs = [...li.children];
-    console.log(`Number of div children in <li>: ${divs.length}`);
-
     divs.forEach((div, index) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
         div.className = 'cards-card-image';
-        console.log(`Div ${index} is an image, assigned class "cards-card-image"`);
       } else if (divs.length === 3 && index === 1) {
         div.className = 'cards-card-title';
-        console.log(`Div ${index} is a title, assigned class "cards-card-title"`);
       } else {
         div.className = 'cards-card-body';
-        console.log(`Div ${index} is a body, assigned class "cards-card-body"`);
       }
     });
-
     ul.append(li);
-    console.log('Appended <li> to <ul>');
   });
   block.textContent = '';
   block.append(ul);
