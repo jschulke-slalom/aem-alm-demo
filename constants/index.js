23f432613f27b577d42d6212ba09d63c9bfa8db3
jschulke-slalom <jacob.schulke@slalom.com>
2024-11-18 09:47:23 -0600

    login

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID = '7c2bd430-45ea-458a-bf73-2c5783a12c91';
+export const CLIENT_SECRET = '58a71211-acfc-4e10-aec8-6c9cde2c309c';
+export const REDIRECT_URI = 'https://main--aem-alm-demo--jschulke-slalom.aem.live';
+export const TOKEN_ENDPOINT = 'https://adobe.com/oauth/token';
