6a2834ae509974757eadc8136ef438a44d76972d
jschulke-slalom <jacob.schulke@slalom.com>
2024-11-18 09:58:13 -0600

    login btn update

--- blocks/login_btn/login_btn.js
+++ blocks/login_btn/login_btn.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const btn = document.createElement('button');
+  const btn = document.createElement('div');
   btn.textContent = 'Login';
   btn.className = 'login-btn';
   block.append(btn);
