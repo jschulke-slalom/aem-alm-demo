a2c4f0090de1c0809bc5d4f5ba750f1784dae74d
jschulke-slalom <jacob.schulke@slalom.com>
2024-11-19 15:15:35 -0600

    login update

--- blocks/login-button/login-button.js
+++ blocks/login-button/login-button.js
@@ -1,21 +1,47 @@
-const redirectUri = 'https://main--aem-alm-demo--jschulke-slalom.aem.live/';
-const authEndpoint = 'https://learningmanager.adobe.com/oauth/o/authorize';
+import {
+  CLIENT_ID, REDIRECT_URI, LOCALREDIRECT_URI, AUTH_ENDPOINT,
+} from '../../constants/index.js';
+
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value};${expires};path=/`;
+}
 
 function login() {
-  const url = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=learner:read,learner:write`;
+  const currentHost = window.location.hostname;
+  const redirect = currentHost === '127.0.0.1' || currentHost === 'localhost' ? LOCALREDIRECT_URI : REDIRECT_URI;
+
+  const url = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=learner:read,learner:write`;
   window.location.href = url;
 }
 
-function checkLoginStatus() {
+function checkLoginStatus(block) {
   // Extract the authorization code from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
 
-  // If there's an authorization code, display a message
+  console.log('Authorization code:', code); // Debugging output
+
+  // If there's an authorization code, display a message and set a cookie
   if (code) {
+    console.log('Code detected, setting logged-in message and cookie.');
+
     const message = document.createElement('div');
     message.textContent = 'Logged in';
-    document.body.appendChild(message);
+    block.appendChild(message);
+
+    // Set a cookie to indicate the user is logged in
+    setCookie('loggedIn', 'true', 7); // Cookie expires in 7 days
+
+    // Hide the login button if logged in
+    const loginButton = block.querySelector('.login-btn');
+    if (loginButton) {
+      loginButton.style.display = 'none';
+    }
+  } else {
+    console.log('No authorization code found.');
   }
 }
 
@@ -31,6 +57,6 @@ export default function decorate(block) {
 
   block.append(btn);
 
-  // Check login status when the component is decorated
-  checkLoginStatus();
+  // Check login status on page load
+  checkLoginStatus(block);
 }
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export const REDIRECT_URI = 'https://main--aem-alm-demo--jschulke-slalom.aem.live/';
+export const LOCALREDIRECT_URI = 'http://127.0.0.1:3000/';
+export const AUTH_ENDPOINT = 'https://learningmanager.adobe.com/oauth/o/authorize';
+export const TOKEN_ENDPOINT = 'https://learningmanager.adobe.com/oauth/o/token';
